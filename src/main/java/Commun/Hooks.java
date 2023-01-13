2e7c480fffa91b47ab59b5355aff3a387923cb9c
Najah <najah.gaddour@gmail.com>
2023-01-13 13:18:24 +0100

    Premiere Version

---
+++ src/main/java/Commun/Hooks.java
@@ -0,0 +1,22 @@
+package Commun;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+    public static WebDriver driver;
+    public void ouvertureApplication() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+
+    }
+    public void closeDriver(){
+        driver.close();
+    }
+}
