d65e0ac4026f7c2f414519fa99597a8d6d86c19c
Pape <papesamba.sarr@live.fr>
2023-01-18 13:35:12 +0100

    Pape modif

--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -1,30 +1,83 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("classpath:features/DescriptionArticle.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("classpath:features/CreationCompte.feature");
 formatter.feature({
-  "name": "[US8]Accès aux détails des articles",
+  "name": "Création de compte client",
   "description": "",
   "keyword": "Feature",
   "tags": [
     {
-      "name": "@REQ_POEC4-15"
+      "name": "@REQ_POEC4-4"
+    }
+  ]
+});
+formatter.scenarioOutline({
+  "name": "Valider la création de compte",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@TEST_POEC4-89"
+    },
+    {
+      "name": "@GR4_TNR"
+    }
+  ]
+});
+formatter.step({
+  "name": "j ouvre l application practice.automationtesting",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "j accède la page My Account",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "je saisis une adresse mail \"\u003cmail\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "je saisis un password \"\u003cPSW\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "Je valide avec le bouton REGISTER",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "J\u0027accède à la page my account",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "mail",
+        "PSW"
+      ]
+    },
+    {
+      "cells": [
+        "groupe4proj@yopmail.com",
+        "Sdqwo8s91!"
+      ]
     }
   ]
 });
 formatter.scenario({
-  "name": "[US8]Accès aux détails des articles",
+  "name": "Valider la création de compte",
   "description": "",
-  "keyword": "Scenario",
+  "keyword": "Scenario Outline",
   "tags": [
     {
-      "name": "@REQ_POEC4-15"
+      "name": "@REQ_POEC4-4"
     },
     {
-      "name": "@TEST_POEC4-33"
+      "name": "@TEST_POEC4-89"
     },
     {
-      "name": "@TESTSET_POEC4-65"
-    },
-    {
-      "name": "@GR4"
+      "name": "@GR4_TNR"
     }
   ]
 });
@@ -39,63 +92,54 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "je clique sur le lien SHOP",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "MyStepdefsDescriptionArticle.jeCliqueSurLeLienSHOP()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "je clique sur un article",
+  "name": "j accède la page My Account",
   "keyword": "When "
 });
 formatter.match({
-  "location": "MyStepdefsDescriptionArticle.jeCliqueSurUnArticle()"
+  "location": "MyStepdefsConnexion.jAccèdeLaPageMyAccount()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "la page detail article s\u0027affiche",
+  "name": "je saisis une adresse mail \"groupe4proj@yopmail.com\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "MyStepdefsCreationCompte.jeSaisisUneAdresseMail(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "je saisis un password \"Sdqwo8s91!\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "MyStepdefsCreationCompte.jeSaisisUnPassword(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Je valide avec le bouton REGISTER",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "MyStepdefsCreationCompte.jeValideAvecLeBoutonREGISTER()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "J\u0027accède à la page my account",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "MyStepdefsDescriptionArticle.laPageDetailArticleSAffiche()"
+  "location": "MyStepdefsCreationCompte.jAccèdeÀLaPageMyAccount()"
 });
 formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "je verifie le bon affichage des details articles",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "MyStepdefsDescriptionArticle.jeVerifieLeBonAffichageDesDetailsArticles()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "je verifie l affichage le bouton ADD TO BASKET",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "MyStepdefsDescriptionArticle.jeVerifieLAffichageLeBoutonADDTOBASKET()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "je verifie un champ permet d indiquer le nombre exemplaire voulu",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "MyStepdefsDescriptionArticle.jeVerifieUnChampPermetDIndiquerLeNombreExemplaireVoulu()"
-});
-formatter.result({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//a[@href\u003d\u0027https://practice.automationtesting.in/my-account/orders/\u0027]\"}\n  (Session info: chrome\u003d108.0.5359.125)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027FRP03387\u0027, ip: \u0027192.168.3.21\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002711.0.17\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 108.0.5359.125, chrome: {chromedriverVersion: 108.0.5359.71 (1e0e3868ee06..., userDataDir: C:\\Users\\FORMAT~1\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:1194}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 37f45db72306b4c272159eafde6b74ac\n*** Element info: {Using\u003dxpath, value\u003d//a[@href\u003d\u0027https://practice.automationtesting.in/my-account/orders/\u0027]}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:490)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat Steps.MyStepdefsCreationCompte.jAccèdeÀLaPageMyAccount(MyStepdefsCreationCompte.java:40)\r\n\tat ✽.J\u0027accède à la page my account(classpath:features/CreationCompte.feature:12)\r\n",
+  "status": "failed"
 });
 });
